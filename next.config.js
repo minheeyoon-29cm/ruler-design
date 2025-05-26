93818679e69bf9ff8b0ed26c678a2a262d631fb4
Minhee Yoon <mini940511@gmail.com>
2025-05-27 02:18:12 +0900

    chore: Netlify 대응을 위한 export 설정 및 build 스크립트 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const { withContentlayer } = require('next-contentlayer');
 
+const isNetlify = process.env.NETLIFY === 'true';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: isNetlify,            // Netlify는 trailing slash 필요
+  output: isNetlify ? 'export' : undefined, // Netlify만 static export
   images: {
+    unoptimized: isNetlify,            // next/image 비활성화 for Netlify
     domains: [
       'd13fzx7h5ezopb.cloudfront.net',
       'img.29cm.co.kr',
