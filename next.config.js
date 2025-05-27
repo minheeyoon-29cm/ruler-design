8124d1a23332800f2e8fe31e2efd8a08f53834ae
Minhee Yoon <mini940511@gmail.com>
2025-05-27 20:18:42 +0900

    fix(config): GitHub Pages 대응을 위한 basePath 및 assetPrefix 설정 추가
    - GitHub Pages에서 /ruler-design 하위 경로에서 정상 렌더링되도록 basePath, assetPrefix 추가
    - CSS, JS, 이미지 경로 로딩 오류 해결 목적

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 const { withContentlayer } = require('next-contentlayer');
 
-const isNetlify = process.env.NETLIFY === 'true';
+const repoName = 'ruler-design';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: isNetlify,            // Netlify는 trailing slash 필요
-  output: isNetlify ? 'export' : undefined, // Netlify만 static export
+  trailingSlash: true,
+  output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
-    unoptimized: isNetlify,            // next/image 비활성화 for Netlify
+    unoptimized: true,
     domains: [
       'd13fzx7h5ezopb.cloudfront.net',
       'img.29cm.co.kr',
